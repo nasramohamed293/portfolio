30d5b1c1cb1f5e66aa0bcb8c60af4c09bbfce28b
nasra mohamed <nasramohamed2120@gmail.com>
2025-07-07 10:06:28 +0300

    my portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+// Simple form submission alert
+const form = document.getElementById("contactForm");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thank you for contacting me! I will get back to you soon.");
+  form.reset();
+});
